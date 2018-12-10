1a1269720fa5882b6f12125a3cb4e7299b8770d0
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-12-10 13:54:55 -0600

    add example `smtcat` for the parser

---
+++ src/batsmt-parser/examples/smtcat.rs
@@ -0,0 +1,41 @@
+
+// A test binary that just parses smtlib and prints it
+
+extern crate env_logger;
+extern crate batsmt_parser;
+extern crate fxhash;
+#[macro_use] extern crate log;
+
+use {
+    std::{env,fs,error::Error},
+    batsmt_parser::{self as parser, simple_ast},
+};
+
+fn main() -> Result<(), Box<Error>> {
+    env_logger::init();
+
+    let mut b = simple_ast::Builder::new();
+
+    // parse
+    let stmts = {
+        let args = env::args();
+        match args.skip(1).next() {
+            None => {
+                info!("parse stdin");
+                parser::parse_stdin(&mut b)?
+            },
+            Some(file) => {
+                info!("parse file {:?}", file);
+                let file = fs::File::open(file)?;
+                parser::parse(&mut b, file)?
+            },
+        }
+    };
+
+    info!("parsed {} statements", stmts.len());
+    for s in &stmts {
+        println!("parsed statement {}", s);
+    }
+
+    Ok(())
+}
