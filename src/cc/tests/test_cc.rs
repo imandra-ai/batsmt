6a5e1d1f9422a571c62a635878c2c46b14a640e1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-01-10 11:23:01 -0600

    test(cc): update tests to also check explanations of propagations

--- src/cc/tests/test_cc.rs
+++ src/cc/tests/test_cc.rs
@@ -210,13 +210,12 @@ mod prop_cc {
     }
 
     // use a naive CC to check this set of lits
-    fn check_lits_sat<I,U>(m: &mut AstGen, b: Builtins<AST>, i: I) -> bool
+    fn check_lits_sat<I,U>(m: &AstGen, i: I) -> bool
         where I: Iterator<Item=U>, U: Into<TermLit>
     {
+        let b = m.b();
         let mut ncc = NaiveCC0::new(b.clone());
 
-        // conflict clause is a tautology,
-        // so assert its negation and check for "unsat"
         for lit in i {
             let lit = lit.into();
             let TermLit(sign,t1,t2) = lit;
@@ -234,6 +233,10 @@ mod prop_cc {
         let ctx = &mut m.0.borrow_mut().m;
         let r = ncc.final_check(ctx);
         r.is_ok()
+    }
+
+    fn check_cube_is_unsat(m: &AstGen, cube: &[TermLit]) -> bool {
+        ! check_lits_sat(m, cube.iter().cloned())
     }
 
     // test that NaiveCC's backtracking behavior is consistent
@@ -278,16 +281,17 @@ mod prop_cc {
                     Op::PartialCheck => (), // do nothing
                     Op::FinalCheck => {
                         // here be the main check
-                        let mut mr = m.0.borrow_mut();
-                        let ctx = &mut mr.m;
-                        let r_ncc = ncc.final_check(ctx);
+                        let r_ncc = {
+                            let mut mr = m.0.borrow_mut();
+                            let ctx = &mut mr.m;
+                            ncc.final_check(ctx)
+                        };
                         let sat1 = r_ncc.is_ok();
-                        drop(mr); // force release of manager
 
                         // check with a fresh ncc, without the push/pop stuff
                         let sat2 = {
                             let lits = st.iter().map(|(_,_,lit)| lit).cloned();
-                            check_lits_sat(m, b.clone(), lits)
+                            check_lits_sat(m, lits)
                         };
 
                         // must agree on satisfiability
@@ -296,7 +300,7 @@ mod prop_cc {
                         // conflict returned by `ncc`, if any, must be valid
                         if let Err(confl) = r_ncc {
                             let lits = confl.0.iter().map(|lit| ! *lit);
-                            let confl_sat = check_lits_sat(m, b.clone(), lits);
+                            let confl_sat = check_lits_sat(m, lits);
 
                             prop_assert!(! confl_sat, "ncc-incremental.conflict is sat");
                         }
@@ -313,6 +317,7 @@ mod prop_cc {
         #[test]
         fn proptest_cc_is_correct(ref tup in with_astgen(|m| cc_ops(m, 120))) {
             let (m, ops) = tup;
+            let mut stack = Stack::new(); // keep current set of ops
 
             //println!("ops: {:?}", ops);
 
@@ -324,23 +329,40 @@ mod prop_cc {
             let mut ncc = NaiveCC0::new(m.b());
             let b = m.b();
 
+            // add literals, for propagations
+            for &op in ops.iter() {
+                match op {
+                    Op::AssertEq(t1,t2) | Op::AssertNeq(t1,t2) => {
+                        let mut mr = m.0.borrow_mut();
+                        let lit = TermLit::mk_eq(t1,t2);
+                        let eqn = mr.mk_app(b.eq, &[t1,t2]);
+                        let ctx = &mut mr.m;
+                        cc.add_literal(ctx, eqn, lit);
+                    },
+                    _ => (),
+                }
+            }
+
             for &op in ops.iter() {
                 match op {
                     Op::PushLevel => {
                         let ctx = &mut m.0.borrow_mut().m;
                         cc.push_level(ctx);
                         ncc.push_level(ctx);
+                        stack.push_level();
                     },
                     Op::PopLevels(n) => {
                         let ctx = &mut m.0.borrow_mut().m;
                         cc.pop_levels(ctx,n);
                         ncc.pop_levels(ctx,n);
+                        stack.pop_levels(n, |_| ());
                     },
                     Op::AssertEq(t1,t2) => {
                         let ctx = &mut m.0.borrow_mut().m;
                         let lit = TermLit::mk_eq(t1,t2);
                         cc.merge(ctx,t1,t2,lit);
                         ncc.merge(ctx,t1,t2,lit);
+                        stack.push(lit);
                     },
                     Op::AssertNeq(t1,t2) => {
                         let lit = TermLit::mk_neq(t1,t2);
@@ -348,32 +370,46 @@ mod prop_cc {
                         let ctx = &mut m.0.borrow_mut().m;
                         cc.merge(ctx,eqn, b.false_, lit);
                         ncc.merge(ctx,eqn, b.false_, lit);
+                        stack.push(lit);
                     },
                     Op::PartialCheck => {
-                        let mut mr = m.0.borrow_mut();
-                        let ctx = &mut mr.m;
-                        let r1 = cc.partial_check(ctx);
+                        let r1 = {
+                            let mut mr = m.0.borrow_mut();
+                            let ctx = &mut mr.m;
+                            cc.partial_check(ctx)
+                        };
 
                         match r1 {
                             Ok(props) => {
+                                let props = props.clone();
+                                drop(r1);
                                 // check each propagation using a copy of `ncc`
                                 for lit in props.iter() {
-                                    check_propagation(m, ncc.clone(), lit);
+                                    check_propagation(m, lit, stack.as_slice());
+                                    let expl = {
+                                        let ctx = &mut m.0.borrow_mut().m;
+                                        let r = cc.explain_prop(ctx, lit);
+                                        drop(ctx);
+                                        r
+                                    };
+                                    check_propagation_expl(m, lit, expl);
                                 }
                             },
                             Err(confl) => {
                                 // check conflict, using a fresh new naiveCC
-                                drop(mr); // release refcell
                                 check_confl(m, &confl.0);
                             }
                         }
                     },
                     Op::FinalCheck => {
                         // here be the main check
-                        let mut mr = m.0.borrow_mut();
-                        let ctx = &mut mr.m;
-                        let r1 = cc.final_check(ctx);
-                        let r2 = ncc.final_check(ctx);
+                        let (r1,r2) = {
+                            let mut mr = m.0.borrow_mut();
+                            let ctx = &mut mr.m;
+                            let r1 = cc.final_check(ctx);
+                            let r2 = ncc.final_check(ctx);
+                            (r1,r2)
+                        };
 
                         // must agree on satisfiability
                         let sat1 = r1.is_ok();
@@ -382,14 +418,22 @@ mod prop_cc {
 
                         match r1 {
                             Ok(props) => {
+                                let props = props.clone();
+                                drop(r1);
                                 // check each propagation using a copy of `ncc`
                                 for lit in props.iter() {
-                                    check_propagation(m, ncc.clone(), lit);
+                                    check_propagation(m, lit, &stack.as_slice());
+                                    let expl = {
+                                        let ctx = &mut m.0.borrow_mut().m;
+                                        let r = cc.explain_prop(ctx, lit);
+                                        drop(ctx);
+                                        r
+                                    };
+                                    check_propagation_expl(m, lit, expl);
                                 }
                             },
                             Err(confl) => {
                                 // check conflict, using a fresh new naiveCC
-                                drop(mr); // release refcell
                                 check_confl(m, &confl.0);
                             }
                         }
@@ -400,54 +444,51 @@ mod prop_cc {
     }
 
     // check that the propagation is valid (ie. ¬b is inconsistent with current trail)
-    fn check_propagation(m: &AstGen, mut ncc: NaiveCC0, lit: TermLit) {
-        let b = m.b();
+    fn check_propagation(m: &AstGen, lit: TermLit, trail: &[TermLit]) {
+        let mut cube = vec![!lit];
+        cube.extend_from_slice(trail);
 
-        // `trail => lit` is a tautology, so assert `¬lit` and check for "unsat"
-        {
-            let TermLit(sign,t1,t2) = ! lit;
-            if sign {
-                let ctx = &mut m.0.borrow_mut().m;
-                ncc.merge(ctx,t1,t2,lit)
-            } else {
-                let eqn = m.app(b.eq, &[t1,t2]); // `t1=t2`
-                let ctx = &mut m.0.borrow_mut().m;
-                ncc.merge(ctx,eqn, b.false_, lit)
-            }
-        }
-
+        let is_unsat = check_cube_is_unsat(m, &cube);
         let ctx = &mut m.0.borrow_mut().m;
-        let r = ncc.final_check(ctx);
+
         //prop_assert!(r.is_err(), "¬lit where lit was propagated should be unsat");
         assert!(
-            r.is_err(), "prop {:?} should be a tauto, but naive cc returned {:?}",
-            pp::debug(lit.pp(ctx)), if r.is_ok() {"sat"} else {"unsat"});
+            is_unsat,
+            "propagation {} should be a tauto in current trail {}, but naive cc returned sat",
+            pp::display(lit.pp(ctx)),
+            pp::display(pp::sexp_iter(trail.iter().map(|x| x.pp(ctx)))),
+            );
+    }
+
+    // check that the propagation's explanation is valid
+    // (ie `expl => lit` is valid, ie `expl & !lit` is unsat)
+    fn check_propagation_expl(m: &AstGen, lit: TermLit, expl: &[TermLit]) {
+        // build the cube `expl & !lit`
+        let mut cube = vec![!lit];
+        for &lit2 in expl.iter() {
+            cube.push(lit2);
+        }
+
+        let is_unsat = check_cube_is_unsat(m, &cube);
+        let ctx = &mut m.0.borrow_mut().m;
+
+        assert!(
+            is_unsat,
+            "for propagation ({} => {})\n\
+            negated cube {} should be unsat, but naive cc returned sat",
+            pp::display(pp::sexp_iter(expl.iter().map(|x| x.pp(ctx)))),
+            lit.pp(ctx),
+            pp::display(pp::sexp_iter(cube.iter().map(|x| x.pp(ctx)))));
     }
 
     // check that the conflict is valid
     fn check_confl(m: &AstGen, confl: &[TermLit]) {
-        let b = m.b();
-        let mut ncc2 = NaiveCC0::new(b.clone());
-
-        // conflict clause is a tautology,
-        for &lit in confl.iter() {
-            let TermLit(sign,t1,t2) = lit;
-            if sign {
-                let ctx = &mut m.0.borrow_mut().m;
-                ncc2.merge(ctx,t1,t2,lit)
-            } else {
-                let eqn = m.app(b.eq, &[t1,t2]); // `t1=t2`
-                let ctx = &mut m.0.borrow_mut().m;
-                ncc2.merge(ctx,eqn, b.false_, lit)
-            }
-        }
-
+        let is_unsat = check_cube_is_unsat(m, confl);
         let ctx = &mut m.0.borrow_mut().m;
-        let r = ncc2.final_check(ctx);
+
         //assert!(r.is_err(), "conflict should be unsat");
         assert!(
-            r.is_err(), "conflict {:?} should be unsat, but naive cc returned {:?}",
-            pp::debug(pp::sexp_iter(confl.iter().map(|x| x.pp(ctx)))),
-            if r.is_ok() {"sat"} else {"unsat"});
+            is_unsat, "conflict {:?} should be unsat, but naive cc returned sat",
+            pp::debug(pp::sexp_iter(confl.iter().map(|x| x.pp(ctx)))));
     }
 }
